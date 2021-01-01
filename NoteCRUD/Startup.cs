69cb7730b0aa1e06395a5baa71e217d006b60873
Daniel Hopson <hdhopson88@yahoo.com>
2021-01-01 14:47:18 -0800

    Scopes repo and service, creates seed data

--- NoteCRUD/Startup.cs
+++ NoteCRUD/Startup.cs
@@ -6,11 +6,14 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using NoteCRUD.Data;
+using NoteCRUD.Services;
 
 namespace NoteCRUD
 {
@@ -28,6 +31,9 @@ namespace NoteCRUD
         {
 
             services.AddControllers();
+            services.AddDbContext<NoteContext>(opt => opt.UseInMemoryDatabase("dbNoteInMemory"));
+            services.AddScoped<INoteRepo, NoteRepo>();
+            services.AddScoped<INoteService, NoteService>();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "NoteCRUD", Version = "v1" });
